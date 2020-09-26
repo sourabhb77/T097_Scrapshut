690e1569268e2be4ce601dc3e43bde207dde50c5
sourabhb77 <bujawadesourabhr@gmail.com>
2020-09-27 00:19:47 +0530

    mergeing branch dhruv

--- helpinghands/accounts/urls.py
+++ helpinghands/accounts/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register ,name='accounts-register'),
+    path('register_ngo/', views.register_ngo ,name='accounts-register-ngo'),
     path('about/', views.about ,name='accounts-about'),
     path('login/', views.login ,name='accounts-login'),
+    path('logout/', views.logout ,name='accounts-logout'),
+    path('home/', views.home ,name='accounts-home'),
+    path('description/', views.description ,name='product-description'),
+
 ]
--- helpinghands/requirement/urls.py
+++ helpinghands/requirement/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     path("" , views.listingListView.as_view(), name= "home"),
-    path("" , views.fillListForm , name ="logout"),
+    path("logout" , views.fillListForm , name ="logout"),
     path("new_listing" , views.fillListForm , name ="listing"),
     path("new_listings" , views.fillListForm , name ="login"),
     path("new_listings2" , views.fillListForm , name ="register"),
